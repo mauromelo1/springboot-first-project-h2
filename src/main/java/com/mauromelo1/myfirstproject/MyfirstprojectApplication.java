2b0579db12bf8a7be2db5a0e01d3fd8f1cc924d8
Mauro Melo <mauromelo1@gmail.com>
2021-03-14 10:07:20 -0300

    Category memory

---
+++ src/main/java/com/mauromelo1/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,32 @@
+package com.mauromelo1.myfirstproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mauromelo1.myfirstproject.entities.Category;
+import com.mauromelo1.myfirstproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+
+	}
+
+}
